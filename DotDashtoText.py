3b896543e7599015268058091fa0527424a308dc
Anfallsigelkott <axelrinaldo@gmail.com>
2024-02-13 17:07:35 +0100

    On Off to Text

---
+++ DotDashtoText.py
@@ -0,0 +1,90 @@
+from enum import Enum
+
+class Cha(Enum):
+    DIT = 1
+    DAT = 2
+    LETTERSPACE = 3
+    SPACE = 4
+
+class DotDashtoText():
+    outputText = ""
+
+    morseTable = {
+        ".-":     "A",
+        "-...":   "B",
+        "-.-.":   "C",
+        "-..":    "D",
+        ".":      "E",
+        "..-.":   "F",
+        "--.":    "G",
+        "....":   "H",
+        "..":     "I",
+        ".---":   "J",
+        "-.-":    "K",
+        ".-..":   "L",
+        "--":     "M",
+        "-.":     "N",
+        "---":    "O",
+        ".--.":   "P",
+        "--.-":   "Q",
+        "-.-":    "R",
+        "...":    "S",
+        "-":      "T",
+        "..-":    "U",
+        "...-":   "V",
+        ".--":    "W",
+        "-..-":   "X",
+        "-.--":   "Y",
+        "--..":   "Z",
+        ".--.-":  "Å",
+        ".-.-":   "Ä",
+        "---.":   "Ö",
+        "-----":  "0",
+        ".----":  "1",
+        "..---":  "2",
+        "...--":  "3",
+        "....-":  "4",
+        ".....":  "5",
+        "-....":  "6",
+        "--...":  "7",
+        "---..":  "8",
+        "----.":  "9",
+        ".-.-.-": ".",
+        "-.-.-.": ";",
+        "--..--": ",",
+        "---...": ":",
+        "-.-.--": "!",
+        "..--..": "?",
+        "-...-":  "=",
+    }
+
+    def __init__(self) -> None:
+        pass
+
+    def recieveSequence(self, dotDashSequence: list):
+        print(dotDashSequence)
+
+        endOfWord = False
+        if dotDashSequence[len(dotDashSequence)-1] == Cha.SPACE:
+            endOfWord = True
+        dotDashSequence.pop()
+
+        dotDashString = ""
+        for cha in dotDashSequence:
+            if cha == Cha.DIT:
+                dotDashString = dotDashString + "."
+            else:
+                dotDashString = dotDashString + "-"
+
+        self.outputText = self.outputText + self.morseTable.get(dotDashString, "*:(*")
+        if endOfWord:
+            self.outputText = self.outputText + " "
+
+        print(self.outputText)
+
+
+
+
+
+
+
---
+++ OnOfftoDotDash.py
@@ -0,0 +1,72 @@
+from DotDashtoText import DotDashtoText, Cha
+
+class OnOfftoDotDash():
+    dotDashtoText = DotDashtoText()
+    wpm = 14
+    outputDotDashSequence = []
+
+    def __init__(self, wpm) -> None:
+        self.wpm = wpm
+
+    def recieveState(self, state, time):
+        if state:
+            if time < self.wpmToms() * 2.5: # TODO: Adjust forgiving timing
+                self.outputDotDashSequence.append(Cha.DIT)
+            else:
+                self.outputDotDashSequence.append(Cha.DAT)
+        else:
+            if time < self.wpmToms() * 2.5:
+                pass # Dot-dash space
+            elif time < self.wpmToms() * 6.5:
+                self.outputDotDashSequence.append(Cha.LETTERSPACE)
+                self.dotDashtoText.recieveSequence(self.outputDotDashSequence)
+                self.outputDotDashSequence.clear()
+            else:
+                self.outputDotDashSequence.append(Cha.SPACE)
+                self.dotDashtoText.recieveSequence(self.outputDotDashSequence)
+                self.outputDotDashSequence.clear()
+
+    def wpmToms(self):
+        return (1200 / self.wpm)
+
+onOff = OnOfftoDotDash(14)    
+
+onOff.recieveState(True, 300)
+onOff.recieveState(False, 300)
+
+onOff.recieveState(True, 100)
+onOff.recieveState(False, 100)
+onOff.recieveState(True, 100)
+onOff.recieveState(False, 100)
+onOff.recieveState(True, 100)
+onOff.recieveState(False, 100)
+onOff.recieveState(True, 100)
+onOff.recieveState(False, 300)
+
+onOff.recieveState(True, 100)
+onOff.recieveState(False, 600)
+
+
+
+onOff.recieveState(True, 100)
+onOff.recieveState(False, 100)
+onOff.recieveState(True, 300)
+onOff.recieveState(False, 600)
+
+onOff.recieveState(True, 300)
+onOff.recieveState(False, 100)
+onOff.recieveState(True, 100)
+onOff.recieveState(False, 100)
+onOff.recieveState(True, 100)
+onOff.recieveState(False, 100)
+onOff.recieveState(True, 100)
+onOff.recieveState(False, 600)
+
+onOff.recieveState(True, 300)
+onOff.recieveState(False, 100)
+onOff.recieveState(True, 100)
+onOff.recieveState(False, 100)
+onOff.recieveState(True, 300)
+onOff.recieveState(False, 100)
+onOff.recieveState(True, 100)
+onOff.recieveState(False, 600)
