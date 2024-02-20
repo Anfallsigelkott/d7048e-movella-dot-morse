eafcb6596bea9f2319828003c91945f7c74f52ff
ChikenUni <hlindgren.hemma@gmail.com>
2024-02-20 11:19:15 +0100

    Moved sigToBin functionality into databank

--- sig2bin.py
+++ sig2bin.py
@@ -1,33 +1,36 @@
 import time
-#import OnOfftoDotDash
+import OnOfftoDotDash
 from typing import Any
 
 
-threshold = 90 # Placeholder angle for activation
 
 class databank:
     state = False
     time = 0
+    dotDash = OnOfftoDotDash()
+    threshold = 90 # Placeholder angle for activation
+
+
     def setState(self, newState):
         self.state = newState
+
     def setTime(self):
         self.time = time()
 
-
-def sigToBin(deg, databank):
-    if deg > threshold: # active state read
-        if not databank.state: # going into active
-            timediff = (time() - databank.time)*1000 # time we spent in passive state (in ms)
-            databank.setState(databank, True) # set state to active
-            databank.setTime(databank) # refresh timestamp
-            # recieveState(False, timediff)
-        # already in active, do nothing
+    def sigToBin(self, deg):
+        if deg > self.threshold: # active state read
+            if not self.state: # going into active
+                timediff = (time() - self.time)*1000 # time we spent in passive state (in ms)
+                self.setState(self, True) # set state to active
+                self.setTime(self) # refresh timestamp
+                self.dotDash.recieveState(self, False, timediff)
+            # already in active, do nothing
             
-    else: # passive state read
-        if databank.state: # going into passive
-            timediff = (time() - databank.time)*1000 # time we spent in active state (in ms)
-            databank.setState(databank, False) # set state to passive
-            databank.setTime(databank) # refresh timestamp
-            #recieveState(True, timediff)
-        # already in passive, do nothing
+        else: # passive state read
+            if self.state: # going into passive
+                timediff = (time() - self.time)*1000 # time we spent in active state (in ms)
+                self.setState(self, False) # set state to passive
+                self.setTime(self) # refresh timestamp
+                self.dotDash.recieveState(self, True, timediff)
+            # already in passive, do nothing
 
